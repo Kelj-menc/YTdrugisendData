0f28e8e750222fe4d027fadf1436a41f09a13a86
Kelj-menc <keljmenc@gmail.com>
2022-09-22 20:53:43 +0200

    Add project files.

---
+++ SignUp.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace YTdrugisendData
+{
+    partial class SignUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtEmailAddress = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.chkSMS = new System.Windows.Forms.CheckBox();
+            this.chkReports = new System.Windows.Forms.CheckBox();
+            this.chkTransactions = new System.Windows.Forms.CheckBox();
+            this.lblSMSMessage = new System.Windows.Forms.Label();
+            this.lblTransactionMessage = new System.Windows.Forms.Label();
+            this.btnSignUp = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(103, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(126, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First name";
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtFirstName.Location = new System.Drawing.Point(235, 70);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(674, 35);
+            this.txtFirstName.TabIndex = 1;
+            // 
+            // txtLastName
+            // 
+            this.txtLastName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtLastName.Location = new System.Drawing.Point(235, 122);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(674, 35);
+            this.txtLastName.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(103, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Last name";
+            // 
+            // txtEmailAddress
+            // 
+            this.txtEmailAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtEmailAddress.Location = new System.Drawing.Point(235, 169);
+            this.txtEmailAddress.Name = "txtEmailAddress";
+            this.txtEmailAddress.Size = new System.Drawing.Size(674, 35);
+            this.txtEmailAddress.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(103, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 29);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Email";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblTransactionMessage);
+            this.groupBox1.Controls.Add(this.lblSMSMessage);
+            this.groupBox1.Controls.Add(this.chkTransactions);
+            this.groupBox1.Controls.Add(this.chkReports);
+            this.groupBox1.Controls.Add(this.chkSMS);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.groupBox1.Location = new System.Drawing.Point(120, 281);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(789, 198);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Services";
+            // 
+            // chkSMS
+            // 
+            this.chkSMS.AutoSize = true;
+            this.chkSMS.Location = new System.Drawing.Point(91, 53);
+            this.chkSMS.Name = "chkSMS";
+            this.chkSMS.Size = new System.Drawing.Size(224, 33);
+            this.chkSMS.TabIndex = 0;
+            this.chkSMS.Text = "SMS notifications";
+            this.chkSMS.UseVisualStyleBackColor = true;
+            this.chkSMS.CheckedChanged += new System.EventHandler(this.chkSMS_CheckedChanged);
+            // 
+            // chkReports
+            // 
+            this.chkReports.AutoSize = true;
+            this.chkReports.Location = new System.Drawing.Point(91, 96);
+            this.chkReports.Name = "chkReports";
+            this.chkReports.Size = new System.Drawing.Size(124, 33);
+            this.chkReports.TabIndex = 1;
+            this.chkReports.Text = "Reports";
+            this.chkReports.UseVisualStyleBackColor = true;
+            this.chkReports.CheckedChanged += new System.EventHandler(this.chkReports_CheckedChanged);
+            // 
+            // chkTransactions
+            // 
+            this.chkTransactions.AutoSize = true;
+            this.chkTransactions.Location = new System.Drawing.Point(91, 139);
+            this.chkTransactions.Name = "chkTransactions";
+            this.chkTransactions.Size = new System.Drawing.Size(268, 33);
+            this.chkTransactions.TabIndex = 2;
+            this.chkTransactions.Text = "Transactions Reports";
+            this.chkTransactions.UseVisualStyleBackColor = true;
+            this.chkTransactions.CheckedChanged += new System.EventHandler(this.chkTransactions_CheckedChanged);
+            // 
+            // lblSMSMessage
+            // 
+            this.lblSMSMessage.AutoSize = true;
+            this.lblSMSMessage.Location = new System.Drawing.Point(394, 57);
+            this.lblSMSMessage.Name = "lblSMSMessage";
+            this.lblSMSMessage.Size = new System.Drawing.Size(31, 29);
+            this.lblSMSMessage.TabIndex = 3;
+            this.lblSMSMessage.Text = "...";
+            // 
+            // lblTransactionMessage
+            // 
+            this.lblTransactionMessage.AutoSize = true;
+            this.lblTransactionMessage.Location = new System.Drawing.Point(394, 143);
+            this.lblTransactionMessage.Name = "lblTransactionMessage";
+            this.lblTransactionMessage.Size = new System.Drawing.Size(31, 29);
+            this.lblTransactionMessage.TabIndex = 4;
+            this.lblTransactionMessage.Text = "...";
+            // 
+            // btnSignUp
+            // 
+            this.btnSignUp.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnSignUp.Location = new System.Drawing.Point(675, 495);
+            this.btnSignUp.Name = "btnSignUp";
+            this.btnSignUp.Size = new System.Drawing.Size(213, 48);
+            this.btnSignUp.TabIndex = 7;
+            this.btnSignUp.Text = "Sign Up";
+            this.btnSignUp.UseVisualStyleBackColor = true;
+            this.btnSignUp.Click += new System.EventHandler(this.btnSignUp_Click);
+            // 
+            // SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1004, 574);
+            this.Controls.Add(this.btnSignUp);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.txtEmailAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtLastName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.label1);
+            this.Name = "SignUp";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtLastName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtEmailAddress;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblTransactionMessage;
+        private System.Windows.Forms.Label lblSMSMessage;
+        private System.Windows.Forms.CheckBox chkTransactions;
+        private System.Windows.Forms.CheckBox chkReports;
+        private System.Windows.Forms.CheckBox chkSMS;
+        private System.Windows.Forms.Button btnSignUp;
+    }
+}
+
---
+++ SignUp.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YTdrugisendData
+{
+    public partial class SignUp : Form
+    {
+        public static string fName;
+        public static string lName;
+        public static string emailAddress;
+
+        public static Boolean sms ;
+        public static Boolean reports;
+        public static Boolean transactions;
+        public SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignUp_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chkSMS_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkSMS.Checked)
+            {
+                sms = true;
+                lblSMSMessage.Text = "SMS Services Charges may Apply";
+            }
+            else
+            {
+                sms = false;
+                lblSMSMessage.Text = "....";
+            }
+        }
+
+        private void chkReports_CheckedChanged(object sender, EventArgs e)
+        {
+            if(chkReports.Checked)
+            {
+                reports = true;
+            }
+            else
+            {
+                reports = false;
+            }
+        }
+
+        private void chkTransactions_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkTransactions.Checked)
+            {
+                transactions = true;
+                lblTransactionMessage.Text = "Charges may Apply for Transaction";
+            }
+            else
+            {
+                transactions = false;
+                lblTransactionMessage.Text = "....";
+
+                ////////Stigao do videa No7!!!!!!!!!!!!!!!!!
+                //////hsdhfh
+
+            }
+        }
+    }
+}
